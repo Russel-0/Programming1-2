930bd323b888ea0c921bddd3d55e73b17aba36fc
russ <117883052+Russel-0@users.noreply.github.com>
2023-10-26 22:27:57 +0800

    save

--- Data-Structure/Hashmap.java
+++ Data-Structure/Hashmap.java
@@ -3,6 +3,15 @@ import java.util.Hashmap;
 
 public static Main {
     public static void (String[] args){
-        System.out.print("Hello World");
+        Scanner sc = new Scanner(System.in);
+        Map<Integer, String[]> Patient = new Hashmap<>();
+
+        System.out.print("Enter number of patient: ");
+        int np = sc.nextInt();
+        sc.nextLine();
+
+        for(int i = 1; i <= np; i++){
+            
+        }
     }
 }
